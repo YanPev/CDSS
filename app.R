89242a389f5c89434ef20e4a2af6b33043d51864
YanPev <131339815+YanPev@users.noreply.github.com>
2025-05-14 09:45:12 +0300

    Final Ver

--- app.R
+++ app.R
@@ -3,42 +3,47 @@ library(readxl)
 library(readr)
 library(dplyr)
 library(DT)
+library(RSQLite)
 library(lubridate)
-library(shinyTime)
 
-# Load patient data
-db <- read_excel("project_db.xlsx", sheet = 1) %>%
-  rename(loinc = `LOINC-NUM`,
-         value = Value,
-         unit = Unit,
-         datetime = `Valid start time`,
-         name = `First name`,
-         lastname = `Last name`) %>%
-  mutate(fullname = paste(name, lastname))
+# Connect to SQLite DB
+if (!file.exists("patient_db.sqlite")) {
+  db_excel <- read_excel("project_db.xlsx") %>%
+    rename(
+      loinc = `LOINC-NUM`,
+      value = Value,
+      unit = Unit,
+      datetime = `Valid start time`,
+      name = `First name`,
+      lastname = `Last name`
+    ) %>%
+    mutate(
+      fullname = paste(name, lastname),
+      datetime = as.numeric(as.POSIXct(datetime))  # store as UNIX timestamp
+    )
 
-db$datetime <- as.POSIXct(db$datetime)
+  con <- dbConnect(SQLite(), "patient_db.sqlite")
+  dbWriteTable(con, "project_db", db_excel, overwrite = TRUE)
+  dbDisconnect(con)
+}
 
 # Load LOINC dictionary
 loinc_dict <- read_csv("Loinc.csv", show_col_types = FALSE) %>%
-  select(loinc = LOINC_NUM, description = COMPONENT)
+  select(loinc = `LOINC_NUM`, loinc_name = COMPONENT)
+
+# Reconnect for session
+con <- dbConnect(SQLite(), "patient_db.sqlite")
 
 ui <- fluidPage(
-  tags$head(
-    tags$style(HTML("body { background-color: pink; }"))
-  ),
-  titlePanel("CDSS System - Part A"),
+  tags$head(tags$style(HTML("body { background-color: pink; }"))),
+  titlePanel("CDSS System - SQL Edition"),
   tabsetPanel(
     tabPanel("View History",
              fluidRow(
                column(4,
-                      selectizeInput("patient_name", "Patient First Name",
-                                     choices = unique(db$name),
-                                     selected = NULL,
-                                     options = list(
-                                       placeholder = 'Start typing a name...',
-                                       maxOptions = 10,
-                                       create = FALSE
-                                     )),
+                      selectizeInput("patient_fullname", "Patient Full Name",
+                                     choices = NULL,
+                                     options = list(placeholder = 'Start typing a name...')),
                       textInput("loinc_code", "LOINC Code (optional)", value = ""),
                       dateInput("from_date", "From Date", value = Sys.Date() - 5),
                       dateInput("to_date", "To Date", value = Sys.Date() + 1),
@@ -55,44 +60,28 @@ ui <- fluidPage(
     tabPanel("Update Measurement",
              sidebarLayout(
                sidebarPanel(
-                 selectizeInput("update_name", "Patient First Name",
-                                choices = unique(db$name),
-                                selected = NULL,
-                                options = list(
-                                  placeholder = 'Start typing a name...',
-                                  maxOptions = 10,
-                                  create = FALSE
-                                )),
+                 selectizeInput("update_fullname", "Patient Full Name", choices = NULL,
+                                options = list(placeholder = 'Start typing a name...')),
                  textInput("update_loinc", "LOINC Code"),
                  dateInput("update_date", "Measurement Date"),
                  timeInput("update_time", "Measurement Time"),
                  numericInput("new_value", "New Value", value = NA),
                  actionButton("update_button", "Update Measurement")
                ),
-               mainPanel(
-                 verbatimTextOutput("update_result")
-               )
+               mainPanel(verbatimTextOutput("update_result"))
              )
     ),
     tabPanel("Delete Measurement",
              sidebarLayout(
                sidebarPanel(
-                 selectizeInput("delete_name", "Patient First Name",
-                                choices = unique(db$name),
-                                selected = NULL,
-                                options = list(
-                                  placeholder = 'Start typing a name...',
-                                  maxOptions = 10,
-                                  create = FALSE
-                                )),
+                 selectizeInput("delete_fullname", "Patient Full Name", choices = NULL,
+                                options = list(placeholder = 'Start typing a name...')),
                  textInput("delete_loinc", "LOINC Code"),
                  dateInput("delete_date", "Date"),
                  timeInput("delete_time", "Time"),
                  actionButton("delete_button", "Delete Measurement")
                ),
-               mainPanel(
-                 verbatimTextOutput("delete_result")
-               )
+               mainPanel(verbatimTextOutput("delete_result"))
              )
     ),
     tabPanel("LOINC Dictionary",
@@ -100,115 +89,89 @@ ui <- fluidPage(
                sidebarPanel(
                  selectizeInput("loinc_lookup", "Search LOINC Code",
                                 choices = unique(loinc_dict$loinc),
-                                selected = NULL,
-                                options = list(
-                                  placeholder = 'Start typing a LOINC code...',
-                                  maxOptions = 10,
-                                  create = FALSE
-                                )),
+                                options = list(placeholder = 'Start typing a LOINC code...')),
                  actionButton("lookup_button", "Search Code")
                ),
-               mainPanel(
-                 tableOutput("loinc_result")
-               )
+               mainPanel(tableOutput("loinc_result"))
              )
     )
   )
 )
 
 server <- function(input, output, session) {
-  rv <- reactiveValues(data = db)
-  
-  # View History
-  history_data <- eventReactive(input$show_history, {
-    req(input$patient_name, input$from_date, input$to_date)
-    
-    from_dt <- ymd_hms(paste(input$from_date, format(input$from_time, "%H:%M:%S")))
-    to_dt   <- ymd_hms(paste(input$to_date, format(input$to_time, "%H:%M:%S")))
-    
-    df <- rv$data %>%
-      filter(name == input$patient_name,
-             datetime >= from_dt,
-             datetime <= to_dt)
-    
-    if (input$loinc_code != "") {
-      df <- df %>% filter(loinc == input$loinc_code)
-    }
-    
-    df <- df %>% left_join(loinc_dict, by = "loinc")
-    
-    cat(">>> View History Triggered <<<\n")
-    cat("Patient:", input$patient_name, "\n")
-    cat("LOINC:", input$loinc_code, "\n")
-    cat("From:", from_dt, "To:", to_dt, "\n")
-    cat("Rows returned:", nrow(df), "\n")
-    
-    df
+  # Update patient fullname choices
+  observe({
+    fullnames <- dbGetQuery(con, "SELECT DISTINCT fullname FROM project_db")$fullname
+    updateSelectizeInput(session, "patient_fullname", choices = fullnames, server = TRUE)
+    updateSelectizeInput(session, "update_fullname", choices = fullnames, server = TRUE)
+    updateSelectizeInput(session, "delete_fullname", choices = fullnames, server = TRUE)
   })
-  
-  output$history_table <- renderDataTable({
-    validate(
-      need(nrow(history_data()) > 0, "No results found. Please check your inputs.")
-    )
-    history_data()
+
+  # View history
+  observeEvent(input$show_history, {
+    tryCatch({
+      from_dt <- as.numeric(as.POSIXct(paste0(input$from_date, " ", format(input$from_time, "%H:%M:%S"))))
+      to_dt   <- as.numeric(as.POSIXct(paste0(input$to_date, " ", format(input$to_time, "%H:%M:%S"))))
+
+      query <- "SELECT *, datetime(datetime, 'unixepoch') AS readable_time FROM project_db WHERE fullname = ? AND datetime BETWEEN ? AND ?"
+      params <- list(input$patient_fullname, from_dt, to_dt)
+
+      if (input$loinc_code != "") {
+        query <- paste(query, "AND loinc = ?")
+        params <- append(params, input$loinc_code)
+      }
+
+      df <- dbGetQuery(con, query, params = params)
+      df$loinc_name <- loinc_dict$loinc_name[match(df$loinc, loinc_dict$loinc)]
+      df <- df %>% mutate(datetime = readable_time) %>% select(-readable_time)
+
+      output$history_table <- renderDataTable({
+        if (nrow(df) == 0) {
+          data.frame(Message = "No results found. Please check your inputs.")
+        } else {
+          df
+        }
+      })
+    }, error = function(e) {
+      output$history_table <- renderDataTable({
+        data.frame(Error = e$message)
+      })
+    })
   })
-  
-  # Update Measurement
-  update_msg <- eventReactive(input$update_button, {
-    req(input$update_name, input$update_loinc, input$update_date, input$update_time, input$new_value)
-    
-    dt_target <- ymd_hms(paste(input$update_date, format(input$update_time, "%H:%M:%S")))
-    
-    index <- which(name == input$update_name &
-                     loinc == input$update_loinc &
-                     datetime == dt_target)
-    
-    if (length(index) == 0) {
-      "No matching measurement found to update"
+
+  # Update measurement
+  observeEvent(input$update_button, {
+    dt_target <- as.numeric(as.POSIXct(paste0(input$update_date, " ", format(input$update_time, "%H:%M:%S"))))
+    df <- dbGetQuery(con, "SELECT rowid, * FROM project_db WHERE fullname = ? AND loinc = ? AND datetime <= ?",
+                     params = list(input$update_fullname, input$update_loinc, dt_target))
+    if (nrow(df) == 0) {
+      output$update_result <- renderPrint("No matching measurement found to update")
     } else {
-      rv$data$value[index] <- input$new_value
-      paste("Value successfully updated to", input$new_value)
+      last_row <- df[which.max(df$datetime), ]
+      dbExecute(con, "UPDATE project_db SET value = ? WHERE rowid = ?",
+                params = list(input$new_value, last_row$rowid))
+      output$update_result <- renderPrint(sprintf("Value successfully updated to %f", input$new_value))
     }
   })
-  
-  output$update_result <- renderPrint({
-    update_msg()
-  })
-  
-  # Delete Measurement
-  delete_msg <- eventReactive(input$delete_button, {
-    req(input$delete_name, input$delete_loinc, input$delete_date, input$delete_time)
-    
-    dt_target <- ymd_hms(paste(input$delete_date, format(input$delete_time, "%H:%M:%S")))
-    before_delete <- nrow(rv$data)
-    
-    rv$data <- rv$data %>%
-      filter(!(name == input$delete_name &
-                 loinc == input$delete_loinc &
-                 datetime == dt_target))
-    
-    after_delete <- nrow(rv$data)
-    
-    if (after_delete < before_delete) {
-      "Measurement successfully deleted"
+
+  # Delete measurement
+  observeEvent(input$delete_button, {
+    dt_target <- as.numeric(as.POSIXct(paste0(input$delete_date, " ", format(input$delete_time, "%H:%M:%S"))))
+    df <- dbGetQuery(con, "SELECT rowid, * FROM project_db WHERE fullname = ? AND loinc = ?",
+                     params = list(input$delete_fullname, input$delete_loinc))
+    df <- df[df$datetime == dt_target, ]
+    if (nrow(df) == 0) {
+      output$delete_result <- renderPrint("No matching measurement found to delete")
     } else {
-      "No matching measurement found to delete"
+      dbExecute(con, "DELETE FROM project_db WHERE rowid = ?", params = list(df$rowid[1]))
+      output$delete_result <- renderPrint("Measurement successfully deleted")
     }
   })
-  
-  output$delete_result <- renderPrint({
-    delete_msg()
-  })
-  
-  # LOINC Lookup
-  loinc_result <- eventReactive(input$lookup_button, {
-    req(input$loinc_lookup)
-    loinc_dict %>%
-      filter(loinc == input$loinc_lookup)
-  })
-  
-  output$loinc_result <- renderTable({
-    loinc_result()
+
+  # LOINC lookup
+  observeEvent(input$lookup_button, {
+    result <- loinc_dict[loinc_dict$loinc == input$loinc_lookup, ]
+    output$loinc_result <- renderTable(result)
   })
 }
 
